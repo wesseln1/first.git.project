6b4390c7f2710cbd572defb396b1c5d940b04257
wesseln1 <nwessel429@gmail.com>
2018-11-05 16:10:30 -0600

    got the html component to post into the console. still have it hidden.

---
+++ book3/chapter3_prac2.js
@@ -0,0 +1,50 @@
+
+    /////////////// New prac
+
+    const hamburger = {
+        name: 'Hamburger',
+        type: 'beef',
+        cooked: false,
+    }
+    const zucchini = {
+        name: 'Zucchini',
+        type: 'vegetable',
+        cooked: false,
+    }
+    const chickenBreast = {
+        name: 'Chicken Breast',
+        type: 'chicken',
+        cooked: false,
+    }
+    const corn = {
+        name: 'Corn',
+        type: 'vegetable',
+        cooked: false,
+    }
+    const steak = {
+        name: 'Steak',
+        type: 'beef',
+        cooked: false,
+    }
+    
+    // An first array containing the objects to be cooked.
+    const uncookedFood = [hamburger, zucchini, chickenBreast, corn, steak];
+    
+    // An empty array that will store the objects after the `grill()` function cooks the food.
+    const cookedFood = [];
+
+    // Your task is to iterate over the array of uncookedFood and invoke the function for each item so that the cookedFood array contains all of the items after they are cooked.
+
+    function grill (currentObject) {
+   
+        currentObject.cooked = true;
+    
+        // Put the cooked food into the appropriate array
+        cookedFood.push(currentObject);
+    };
+
+    for(let i = 0; i < uncookedFood.length; i++) {
+        grill(uncookedFood[i]);
+        console.log(cookedFood);
+    }
+
--- daily-journal/src/scripts/journal.js
+++ daily-journal/src/scripts/journal.js
@@ -3,17 +3,40 @@ const journalEntries = [
         date: "07/24/2018",
         concept: "Array methods",
         entry: "We learned about 4 different array methods today. forEach made sense, but the others still confuse me.",
-        mood: "Ok"
+        mood: "Ok",
+    },
+
+    {
+        date: "10/24/2018",
+        concept: "Funtions and Logic",
+        entry: "Today we discussed funtions and logic. We went over how to write functions as well as the logic in a functions. Functions can also include for loops and forEach loops.",
+        mood: "Happy, Happy, Happy!"
+    },
+
+    {
+        date: "10/29/2018",
+        concept: "Using and fetching data",
+        etry: "We got to see how to use and call an API for larager amaounts of data. We also got to learn how to make a fecth to the API.",
+        mood: "Wonderful"
     }
     
-let jourEnt = [date, concept, entry, mood];
+]
 
-let 4 = length;
+/*
+    Purpose: To create, and return, a string template that
+    represents a single journal entry object as HTML
 
-for  (let i = 0; i < 4; ++i) {
-    let date = ary[0]
-    let concept = ary[1]
-    let entry = sry[2]
-    let mood = ary[3]
-
+    Arguments: journalEntry (object)
+*/
+const makeJournalEntryComponent = (journalEntry) => {
+    // Create your own HTML structure for a journal entry
+    journalEntry += journalEntries 
+    return `<h1 class="${journalEntry}"></h1>`
 }
+
+const h1 = (title, style) => {
+    return `<h1 class="${style}">${title}</h1>`
+}
+
+const container = document.querySelector(".journal_entry")
+container.innerHTML = makeJournalEntryComponent(journalEntries[1]);
